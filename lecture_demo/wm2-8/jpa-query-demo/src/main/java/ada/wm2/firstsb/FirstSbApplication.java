ce9f70f1cf8006ac0a70867ff83a0c3464820e58
jhasanov <jhasanov@gmail.com>
2021-06-16 10:56:03 +0400

    JPA demo updated

---
+++ lecture_demo/wm2-8/jpa-query-demo/src/main/java/ada/wm2/firstsb/FirstSbApplication.java
@@ -0,0 +1,13 @@
+package ada.wm2.firstsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSbApplication.class, args);
+	}
+
+}
