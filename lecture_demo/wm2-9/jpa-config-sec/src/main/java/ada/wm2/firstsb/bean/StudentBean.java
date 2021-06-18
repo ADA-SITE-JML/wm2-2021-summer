fa4b44fec6bcc1da17032c148be9c0dbd7971151
jhasanov <jhasanov@gmail.com>
2021-06-18 09:47:17 +0400

    jpa-config-security demo added

---
+++ lecture_demo/wm2-9/jpa-config-sec/src/main/java/ada/wm2/firstsb/bean/StudentBean.java
@@ -0,0 +1,27 @@
+package ada.wm2.firstsb.bean;
+
+public class StudentBean {
+    private String firstName;
+    private String lastName;
+
+    public StudentBean(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
