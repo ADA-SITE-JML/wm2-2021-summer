8a854020d923dd652bff8d5b3285a990834a5234
jhasanov <jhasanov@gmail.com>
2021-06-23 09:05:16 +0400

    Logging part added.

--- lecture_demo/wm2-10/error-logging/src/main/java/ada/wm2/firstsb/controller/CustomErrorController.java
+++ lecture_demo/wm2-10/error-logging/src/main/java/ada/wm2/firstsb/controller/CustomErrorController.java
@@ -1,5 +1,7 @@
 package ada.wm2.firstsb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestHandler;
@@ -11,13 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    Logger log = LoggerFactory.getLogger("CustomErrorController");
+
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-            System.out.println("statusCode: "+statusCode);
+
+            log.debug(request.getRequestURI()+" statusCode: "+statusCode);
+
             if (statusCode == 404)
                 return "errorpages/error_404";
             else if (statusCode == 400)
