addfb826921637c5cc795805886e916b8c3ba3f6
frasulov <frasulov4671@ada.edu.az>
2021-06-25 12:16:33 +0400

    submission by Fagan

---
+++ homeworks/hw2/Blog/src/main/java/com/hw2/blog/entity/Post.java
@@ -0,0 +1,121 @@
+package com.hw2.blog.entity;
+
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "POSTS")
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name="id") private Integer id;
+
+
+    @NotNull @Column(name="title") private String title;
+
+    @Column(name = "body", columnDefinition = "text") private String body;
+
+    @Column(name = "image", columnDefinition = "text") private String image;
+
+
+    @CreationTimestamp @Column(name = "created_at",columnDefinition="timestamp default now()") private Timestamp created_at;
+
+    @UpdateTimestamp @Column(name = "updated_at", columnDefinition="timestamp default now()") private Timestamp updated_at;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name="profile_id")
+    private Profile author;
+
+    // i have added unique constraints, becuase (profile, post) must be unique, one user can like one post only once
+    @ManyToMany
+    @JoinTable(name = "PROFILE_LIKED_POSTS",
+                joinColumns = @JoinColumn(name = "POST_ID"),
+                inverseJoinColumns = @JoinColumn(name = "PROFILE_ID"),
+                uniqueConstraints = {
+                    @UniqueConstraint(name = "uniquePostIdProileId",
+                    columnNames = {"POST_ID", "PROFILE_ID"})
+                }
+    )
+    private Set<Profile> liked_profiles = new HashSet<>();
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Profile getAuthor() {
+        return author;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setAuthor(Profile author) {
+        this.author = author;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Set<Profile> getLiked_profiles() {
+        return liked_profiles;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Post(){
+
+    }
+
+    public Post(Profile author){
+        this.author = author;
+    }
+
+    public boolean equalsTo(Post post) {
+        return (this.id == post.id);
+    }
+}
