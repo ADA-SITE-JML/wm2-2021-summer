c846f44fd9bc73e46989e61e603eecdc945911d8
User <mnovruzov2021@ada.edu.az>
2021-06-30 18:52:34 +0400

    A

--- home_practice/mongodb_practice/src/main/java/ada/wm2/mongodb_practice/entity/Student.java
+++ home_practice/mongodb_practice/src/main/java/ada/wm2/mongodb_practice/entity/Student.java
@@ -1,2 +1,42 @@
-package ada.wm2.mongodb_practice.entity;public class Student {
+package ada.wm2.mongodb_practice.entity;
+
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "students")
+public class Student {
+
+    @Id
+    private String id;
+
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+
+
+    private Double gpa;
+
+
+
+    private String password;
+
+
+
+
+
+
 }
